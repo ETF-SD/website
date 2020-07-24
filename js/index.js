cb879a668588a7d2f4a45d0fc2cc98df817d7e99
Safin Singh <safin.singh@gmail.com>
2020-07-24 16:47:48 -0700

    Navbar, lander, and green slant

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 //DOM Elements
-var hours_num = document.getElementById("hours-num")
-var classes_num = document.getElementById("classes-num")
-var students_num = document.getElementById("students-num")
+var hours_num = document.getElementById("hours-num");
+var classes_num = document.getElementById("classes-num");
+var students_num = document.getElementById("students-num");
 
 setTimeout(function () {
-  hours_num.innerHTML = 120
-},500)
+  hours_num.innerHTML = 120;
+}, 500);
 setTimeout(function () {
-  classes_num.innerHTML = 96
-},500)
+  classes_num.innerHTML = 4;
+}, 500);
 setTimeout(function () {
-  students_num.innerHTML = 60
-},500)
+  students_num.innerHTML = 200;
+}, 500);
