cd7e94822ab773428839746a4385f40b07622e4f
Aadit Gupta <spacegiant21@gmail.com>
2020-07-24 21:13:24 -0700

    Fix Numbers

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ var classes_num = document.getElementById("classes-num");
 var students_num = document.getElementById("students-num");
 
 setTimeout(function () {
-  hours_num.innerHTML = 120;
+  hours_num.innerHTML = 110;
 }, 500);
 setTimeout(function () {
   classes_num.innerHTML = 4;
 }, 500);
 setTimeout(function () {
-  students_num.innerHTML = 200;
+  students_num.innerHTML = 170;
 }, 500);
