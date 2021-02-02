09bfabbf754918cdf1044685fd57d4a54310ab6e
Aadit Gupta <spacegiant21@gmail.com>
2021-02-02 09:22:07 -0800

    Update stats

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ var classes_num = document.getElementById("classes-num");
 var students_num = document.getElementById("students-num");
 
 setTimeout(function () {
-  hours_num.innerHTML = 145;
+  hours_num.innerHTML = 152;
 }, 500);
 setTimeout(function () {
-  classes_num.innerHTML = 11;
+  classes_num.innerHTML = 12;
 }, 500);
 setTimeout(function () {
-  students_num.innerHTML = 270;
+  students_num.innerHTML = 470;
 }, 500);
